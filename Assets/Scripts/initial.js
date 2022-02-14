3a9afad2a1c382412e404b5429d5693f186e49c3
GrantGrinols <grantgrinols@oakland.edu>
2022-02-14 14:32:32 -0500

    rotation

--- Assets/Scripts/initial.js
+++ Assets/Scripts/initial.js
@@ -1,59 +1,50 @@
+//Preconditions: The player hits a key indictacting they wish to rotate the currentTetromino
+//postconditions: The x and y values of the current Tetromino are exchanged to simulate a rotation
 
-    var world = [
-        [0,0,0,1,1,0,0,0],
-        [0,0,0,1,1,0,0,0],
-        [0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0],
-    ];
-    function drawWorld() {
-        document.getElementById('world').innerHTML = "";
-        for(var y=0; y<world.length; y++){
-            //console.log(world[y]);
-            for(var x=0; x<world[y].length; x++){
-                //console.log(world[y][x]);
-                if(world[y][x] === 0){
-                    document.getElementById('world').innerHTML += "<div class = 'empty'></div>"
-                } else if(world[y][x] === 1 || world[y][x] === 11){ //11 means that we are freezing it so the block does not fall further
-                    document.getElementById('world').innerHTML += "<div class ='squareShape'></div>"
-                }
-            }
-            document.getElementById('world').innerHTML += "<br>"
-        } 
+function RotateTetromino()
+{
+    let newRotation = new Array();//the function will use this to replace curTetromino
+    let tetrominoCopy = curTetromino; //a copy of the currentTetromino. we're using a copy to prevent errors
+    let curTetrominoBU;//this will carry the origin Tetramino. We will call this if there is an error
+
+    for(let i = 0; i < tetrominoCopy.length; i++)
+    {
+       //construction of the backupTetrmino
+        curTetrominoBU = [...curTetromino];
+
+       //In order to exchange the x and y values, we will need a x value to use as a reference point. In other words, the 'center' of the tetromino
+        let x = tetrominoCopy[i][0];
+        let y = tetrominoCopy[i][1];
+        let newX = (GetLastSquareX() - y);
+        let newY = x;
+        newRotation.push([newX, newY]);//the newRotation is now the currentTetromino
     }
+    DeleteTetromino();//delete the current Tetromino
 
-    function moveShapesDown(){
-        var canMove = true;
-        for(var y=0; y<world.length; y++){ 
-            for(var x=0; x<world[y].length; x++){
-                if(world[y][x] > 0 && world[y][x] < 10){
-                    if(y === world.length-1 || world[y+1][x] > 10){ //checking to see if block is at the end of screen
-                        canMove = false;
-                    }
-                }
-            }
-        }
-        if(canMove){
-            for(var y=world.length-1; y>=0; y--){  
-                for(var x=0; x<world[y].length; x++){
-                    if (world[y][x]>0 && world[y][x] <10){
-                        world[y+1][x] = world[y][x];
-                        world[y][x] = 0;
-                    }
-                }
-            }
+    // and Try to draw the new Tetromino rotation
+    try{
+        curTetromino = newRotation;
+        DrawTetromino();
+    }  
+    //sometimes drawing the Tetromino may now work, such as an out of bounds. In which case the rotation does not work
+    catch (e){ 
+        if(e instanceof TypeError) {
+            curTetromino = curTetrominoBU;
+            DeleteTetromino();
+            DrawTetromino();
         }
     }
-    function gameLoop(){
-        setTimeout(gameLoop, 1000);
+}
+
+//this function returns the relative x value we are focusing the rotation on.
+function GetLastSquareX()
+{
+    let lastX = 0;
+     for(let i = 0; i < curTetromino.length; i++)
+    {
+        let square = curTetromino[i];
+        if (square[0] > lastX)
+            lastX = square[0];
     }
-    drawWorld();
-    moveShapesDown();
-    drawWorld();
+    return lastX;
+}
