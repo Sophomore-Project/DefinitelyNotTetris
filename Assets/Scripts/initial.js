f0e44ed4032071536b28bf4609aaf08e2cab2241
Mesudc <40904758+mesudc@users.noreply.github.com>
2022-02-15 21:38:46 -0500

    Adding rotation functionality that was recently merged to main prior to creating a pull request

--- Assets/Scripts/initial.js
+++ Assets/Scripts/initial.js
@@ -140,7 +140,7 @@ function HandleKeyPress(key){
     //KeyCode 38 is for up arrowkey
     else if(key.keyCode == 38){
         console.log("Rotation function to be called here");
-        //RotateTetromino();
+        RotateTetromino();
     }
 }
 //moves the tetromino down every second
@@ -243,4 +243,51 @@ function HitTheBottom(){
     //return collision
     return collision;
 
+}
+function RotateTetromino()
+{
+    let newRotation = new Array();//the function will use this to replace curTetromino
+    let tetrominoCopy = curTetromino; //a copy of the currentTetromino. we're using a copy to prevent errors
+    let curTetrominoBU;//this will carry the origin Tetramino. We will call this if there is an error
+
+    for(let i = 0; i < tetrominoCopy.length; i++)
+    {
+       //construction of the backupTetrmino
+        curTetrominoBU = [...curTetromino];
+
+       //In order to exchange the x and y values, we will need a x value to use as a reference point. In other words, the 'center' of the tetromino
+        let x = tetrominoCopy[i][0];
+        let y = tetrominoCopy[i][1];
+        let newX = (GetLastSquareX() - y);
+        let newY = x;
+        newRotation.push([newX, newY]);//the newRotation is now the currentTetromino
+    }
+    DeleteTetromino();//delete the current Tetromino
+
+    // and Try to draw the new Tetromino rotation
+    try{
+        curTetromino = newRotation;
+        DrawTetromino();
+    }  
+    //sometimes drawing the Tetromino may now work, such as an out of bounds. In which case the rotation does not work
+    catch (e){ 
+        if(e instanceof TypeError) {
+            curTetromino = curTetrominoBU;
+            DeleteTetromino();
+            DrawTetromino();
+        }
+    }
+}
+
+//this function returns the relative x value we are focusing the rotation on.
+function GetLastSquareX()
+{
+    let lastX = 0;
+     for(let i = 0; i < curTetromino.length; i++)
+    {
+        let square = curTetromino[i];
+        if (square[0] > lastX)
+            lastX = square[0];
+    }
+    return lastX;
 }
