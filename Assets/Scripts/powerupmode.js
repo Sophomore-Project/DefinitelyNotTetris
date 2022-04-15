d0d63e5402000aef7fc955e8f221a32723b328c5
Mesud Cengiz <mesuderkam@gmail.com>
2022-04-15 12:05:32 -0400

    path changes to music files

--- Assets/Scripts/powerupmode.js
+++ Assets/Scripts/powerupmode.js
@@ -321,13 +321,13 @@ function FindGhost() {
 }
 function Track1() {
     let myMusic;
-      myMusic = new Audio("/Assets/Music/tetris-gameboy-01.mp3");
+      myMusic = new Audio("/DefinitelyNotTetris/Assets/Music/tetris-gameboy-01.mp3");
       myMusic.play();
       setTimeout(Track2,39000); 
     }
     function Track2() {
         let myMusic;
-          myMusic = new Audio("/Assets/Music/tetris-gameboy-05.mp3");
+          myMusic = new Audio("/DefinitelyNotTetris/Assets/Music/tetris-gameboy-05.mp3");
           myMusic.play();
           setTimeout(Track1,57000); 
         }
